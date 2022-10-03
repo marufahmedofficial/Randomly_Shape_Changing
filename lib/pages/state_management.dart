509f2dbe7d4bbd0b175dce8a06cec425467d930d
maruf <mk6116055@gmail.com>
2022-10-04 02:21:50 +0600

    Initial commit

---
+++ lib/pages/state_management.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+class StateManagement extends StatefulWidget {
+  const StateManagement({Key? key}) : super(key: key);
+
+  @override
+  State<StateManagement> createState() => _StateManagementState();
+}
+
+class _StateManagementState extends State<StateManagement> {
+  double _width = 100;
+  double _height = 100;
+  final random = Random.secure();
+  int a = 250, r = 200, g = 200, b = 200;
+  double topleft = 10;
+  double topright = 10;
+  double buttomleft = 10;
+  double buttomright = 10;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Management'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeContainer,
+        child: Text('Change'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(topleft),
+              topRight: Radius.circular(topright),
+              bottomLeft: Radius.circular(buttomleft),
+              bottomRight: Radius.circular(buttomright),
+            ),
+            color: Color.fromARGB(a, r, g, b),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  void _changeContainer() {
+    setState(() {
+      _width = random.nextInt(251)+50.toDouble();
+      _height = random.nextInt(251)+50.toDouble();
+      topleft = random.nextInt(251)+50.toDouble();
+      topright = random.nextInt(251)+50.toDouble();
+      buttomleft = random.nextInt(251)+50.toDouble();
+      buttomright = random.nextInt(251)+50.toDouble();
+      a = random.nextInt(255);
+      r = random.nextInt(255);
+      g = random.nextInt(255);
+      b = random.nextInt(255);
+
+    });
+  }
+}
